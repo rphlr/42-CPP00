1ee9b5fa2da9967417ee2851fe9f349c47fb64d5
Raphael Rouiller <rrouille@c1r1s7.42lausanne.ch>
2023-11-25 13:43:11 +0100

    auto commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 13:25:11 by rrouille          #+#    #+#             */
+/*   Updated: 2023/11/25 13:41:10 by rrouille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1) std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+        for (int i = 1; i < argc; i++)
+            for (int j = 0; argv[i][j]; j++)
+                std::cout << (char)std::toupper(argv[i][j]);
+    std::cout << std::endl;
+    return (0);
+}
