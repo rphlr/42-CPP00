ca072f1560227058bba9ef27f07777edbe02493f
Raphael Rouiller <rrouille@c1r1s7.42lausanne.ch>
2023-12-09 14:55:01 +0100

    improved exs

--- ex02/srcs/Account.cpp
+++ ex02/srcs/Account.cpp
@@ -6,11 +6,40 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:55:03 by rrouille          #+#    #+#             */
-/*   Updated: 2023/12/09 11:39:44 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/12/09 14:54:10 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+// Terminal color settings for output messages
+const std::string GRAY		= "\033[0;90m";
+const std::string RED		= "\033[0;91m";
+const std::string GREEN		= "\033[0;92m";
+const std::string YELLOW	= "\033[0;93m";
+const std::string BLUE		= "\033[0;94m";
+const std::string MAGENTA	= "\033[0;95m";
+const std::string CYAN		= "\033[0;96m";
+const std::string WHITE		= "\033[0;97m";
+const std::string ENDCOLOR	= "\033[0m";
+
+// Background colors for terminal
+const std::string BG_G		= "\033[42m";
+const std::string BG_R		= "\033[41m";
+const std::string BG_Y		= "\033[43m";
+const std::string BG_B		= "\033[44m";
+const std::string BG_M		= "\033[45m";
+const std::string BG_C		= "\033[46m";
+const std::string BG_W		= "\033[47m";
+
+// Text formatting options for output messages
+const std::string BOLD		= "\033[1m";
+const std::string UNDERLINE	= "\033[4m";
+const std::string ITALIC	= "\033[3m";
+const std::string INVERTED	= "\033[7m";
 
 int					Account::_nbAccounts			= 0;
 int					Account::_totalAmount			= 0;
